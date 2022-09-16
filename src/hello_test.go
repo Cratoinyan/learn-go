f15272147ef6bb2625f2cb46de04915d0a5dce05
Umut_Ciloglu <umutciloglu06@gmail.com>
2022-09-16 16:33:01 +0300

    add french and spanish language options for hello world

--- src/hello_test.go
+++ src/hello_test.go
@@ -4,14 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
+
 	t.Run("empty stirng defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello in Spanish", func(t *testing.T) {
+		got := Hello("Jose", "Spanish")
+		want := "Hola, Jose"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello in French", func(t *testing.T) {
+		got := Hello("Napoleon", "French")
+		want := "Bonjour, Napoleon"
 
 		assertCorrectMessage(t, got, want)
 	})
